0f13103eaf7d011a18485df422da30ef8a6e804e
Bader <bjeragh@email.arizona.edu>
2019-04-07 11:35:34 -0700

    Basic Switch

---
+++ lib/switch/switch.cpp
@@ -0,0 +1,10 @@
+// PB1 Switch
+#include "switch.h"
+#include <avr/io.h>
+
+void initSwitchPB1(){
+  DDRB &= ~(1 << DDB1); // Initializes pull-up resistor on PB3 and sets it into input mode
+  PORTB |= (1 << PORTB1); // enable pull-up for PORTB3
+
+  PCICR |= (1 << PCIE0);      // enable PCINT for 7-0
+  PCMSK0 |= (1 << PCINT3);    // enable PCINT for PCINT3
